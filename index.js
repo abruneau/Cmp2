43211da5704dea489ea6cb6619782776dae9e48e
Antonin Bruneau <abruneau@cloudera.com>
2017-02-15 18:09:51 +0100

    Set dock icon

--- index.js
+++ index.js
@@ -3,51 +3,52 @@ var app, electron;
 
 electron = require('electron');
 
-
 app = electron.app;
 
 var mainWindow = null;
 
 function createWindow() {
-	// Create the browser window.
-	mainWindow = new electron.BrowserWindow({
-		width: 800,
-		height: 600
-	});
+  // Create the browser window.
+  mainWindow = new electron.BrowserWindow({
+    title: 'Cmp2',
+    width: 800,
+    height: 600,
+    icon: __dirname + 'app/images/icons/png/64x64.png'
+  });
 
-	// and load the index.html of the app.
-	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 
-	// Open the DevTools.
-	// mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
 
-	// Emitted when the window is closed.
-	mainWindow.on('closed', function () {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 
 }
 
-app.on('ready', function () {
-	createWindow();
+app.on('ready', function() {
+  createWindow();
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function () {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
