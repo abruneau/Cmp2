50096ad431034b3d64cb7e9a4eaf3a29458fae8b
Antonin Bruneau <abruneau@cloudera.com>
2017-02-24 11:05:07 +0100

    Get info from SFDC about forecast

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,25 @@
  * # MainCtrl
  * Controller of the cmp2App
  */
-angular.module('cmp2App').controller('MainCtrl', function() {
-  // if (!salesForce.sfSet()) {
-  // 	$window.location.href = '#/profile';
-  // }
+angular.module('cmp2App').controller('MainCtrl', function(salesForce, $scope) {
+
+
+  function loadForcast() {
+    salesForce.loadForcastBoard().then(function(res) {
+      if (res) {
+        $scope.$apply(function() {
+          $scope.forcastBoard = res.records[0];
+          console.log($scope.forcastBoard);
+        });
+      }
+    });
+  }
+
+
+  var updateSf = function() {
+    loadForcast();
+  };
+
+  loadForcast();
+  salesForce.registerObserverCallback(updateSf);
 });
